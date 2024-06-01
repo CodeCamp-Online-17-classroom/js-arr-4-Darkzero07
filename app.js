9611d98f35e917236a0d922cc5e0ea26a3dbfc5f
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-01 23:38:27 +0700

    js lab 90

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// write code here
+const prompt = require("prompt-sync")();
+let arr = new Array();
+
+while (true) {
+  const num = prompt("Enter number: ");
+  if (isNaN(num)) {
+    break;
+  }
+  arr.push(parseFloat(num));
+}
+
+let sum = arr.reduce((acc, cur) => acc + cur);
+
+console.log(arr);
+console.log(sum);
